26a161b5f03bbaac449e2a13ec7cf17651a2498d
temalcode <79495831+temalcode@users.noreply.github.com>
2021-10-28 13:25:11 +0530

    Create leftTranglePattern.java

---
+++ leftTranglePattern.java
@@ -0,0 +1,28 @@
+class leftTranglePattern {
+    
+    public static void main(String[] args) {
+        
+        int num = 5;
+
+
+        for (int i = 1; i <= num; i++){
+            for(int j = 0; j < i; j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = num-1; i > 0; i-- ){
+            for(int j=0; j<i; j++)
+            {
+                System.out.print("*");
+            }    
+
+            System.out.println();
+        }
+
+        
+    }
+}
