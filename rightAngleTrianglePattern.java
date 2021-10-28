d695a058d1feb364a505288741b9c9d58f794581
temalcode <79495831+temalcode@users.noreply.github.com>
2021-10-28 13:25:29 +0530

    Create rightAngleTrianglePattern.java

---
+++ rightAngleTrianglePattern.java
@@ -0,0 +1,23 @@
+class rightAngleTrianglePattern{
+
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+
+    public static void main(String[] args) {
+        
+        int num = 5;
+
+        for(int i=1; i<=num; i++){
+
+            for (int j= 0; j<i; j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
